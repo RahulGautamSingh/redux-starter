73b0227d730eb38474c9ff0c6739a40ae9a8eef4
Rahul Gautam Singh <rahul_ug@cse.nits.ac.in>
2021-07-11 21:46:59 +0545

    updated default value of campnay Name

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -225,6 +225,7 @@ export default function HomePage() {
                     setEmployeeCompanyErr("Cannot be blank");
                   else setEmployeeCompanyErr("");
                 }}
+                defaultValue={companies[0]}
               >
                 {state.companies.map((elem, index) => {
                 
